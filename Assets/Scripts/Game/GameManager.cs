dec8302d45efa76797f5f0bd7e17e43e0db42243
Andrew Leavitt <andyleavitt@gmail.com>
2017-08-17 22:45:50 -0600

    Approaching a working game manager?

---
+++ Assets/Scripts/Game/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager instance = null;
+
+    private LevelManager levelScript;
+
+    void Awake() {
+        if (instance == null) {
+            instance = this;
+        } else {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+        levelScript = GetComponent<LevelManager>();
+        InitGame();
+    }
+
+    void InitGame() {
+        levelScript.SetupLevel();
+    }
+}
---
+++ Assets/Scripts/Game/LevelManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+    public EnemySpaceShip enemy;
+
+    internal void SetupLevel() {
+        enemy = new EnemySpaceShip();
+        LayoutObject(enemy);
+    }
+
+    private void LayoutObject(EnemySpaceShip enemy) {
+        Instantiate(enemy, new Vector3(9f, 0f, 0f), Quaternion.identity);
+    }
+}
