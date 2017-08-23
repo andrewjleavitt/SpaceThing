1bdc0fb4fb1660f6a9876f2d0dd737ce98c69301
Andrew Leavitt <andyleavitt@gmail.com>
2017-08-22 21:29:57 -0600

    Revert "Merge pull request #2 from andrewjleavitt/revert-1-gamemanager"
    This reverts commit fd8b121159aa65310f0369a3d04b3834be203be5, reversing
    changes made to 31c306d2ea63d48a0b72e8f1a6e3526b1945e41e.

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+    public EnemySpaceShip enemy;
+
+    public void SetupLevel() {
+        LayoutObject();
+    }
+
+    private void LayoutObject() {
+        Instantiate(enemy, new Vector3(9f, 0f, 0f), Quaternion.identity);
+    }
+}
