c18501fbcda6f271c5a6f2cf0acd2848c39acd48
Andrew Leavitt <andyleavitt@gmail.com>
2017-09-02 08:57:22 -0600

    move slider behavior into slider controller

--- Assets/Scripts/GunBehavior.cs
+++ Assets/Scripts/GunBehavior.cs
@@ -5,31 +5,29 @@ public class GunBehavior : MonoBehaviour {
     public float fireRate = 0.0f;
     public float heat = 0.0f;
     public string weaponName;
-	public Slider charge;
+    public Slider charge;
 
     public float nextFire = 0.0f;
     public float heatBuildUp = 0.0f;
 
-	private Slider sliderController;
+    private Slider slider;
+    private SliderController sliderScript;
 
     public GunBehavior(string newWeaponName, float newFireRate, float newHeat) {
         weaponName = newWeaponName;
         fireRate = newFireRate;
         heat = newHeat;
-	}
+    }
 
-	void Start() {
-		GameObject canvas = GameObject.Find ("CanvasRenderer");
-		GameObject sliderObject = Instantiate (Resources.Load("GunCooldownSlider"), canvas.transform) as GameObject;
-		sliderController = sliderObject.GetComponent<Slider> ();
-	}
-
-	void Update() {
-		setSliderValue ();
-	}
+    void Start() {
+        GameObject canvas = GameObject.Find("CanvasRenderer");
+        GameObject sliderObject = Instantiate(Resources.Load("GunCooldownSlider"), canvas.transform) as GameObject;
+        slider = sliderObject.GetComponent<Slider>();
+        sliderScript = slider.GetComponent<SliderController>();
+    }
 
     public void Trigger() {
-        if(Time.time > nextFire) {
+        if (Time.time > nextFire) {
             Fire();
         }
     }
@@ -44,15 +42,10 @@ public class GunBehavior : MonoBehaviour {
         Debug.unityLogger.Log("INFO", string.Format("Gun: {0} Triggered", weaponName));
         Exhaust();
         nextFire = Time.time + fireRate;
-		sliderController.minValue = Time.time;
-		sliderController.maxValue = nextFire;
+        sliderScript.setMinMax(Time.time, nextFire);
     }
 
     private void Exhaust() {
         heatBuildUp += heat; ;
     }
-
-	private void setSliderValue() {
-		sliderController.value = Time.time;
-	}
 }
