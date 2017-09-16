9efc283c9597e79fa74c32ecc5943db38068b8a9
Andrew Leavitt <andyleavitt@gmail.com>
2017-09-15 22:07:58 -0600

    Missile chases closest enemy

--- Assets/MissileController.cs
+++ Assets/MissileController.cs
@@ -7,12 +7,40 @@ public class MissileController : MonoBehaviour {
     public float thrust = 5.0f;
 
     private Rigidbody2D rb;
+    private GameObject target;
 
 	void Start () {
-        rb = gameObject.GetComponent<Rigidbody2D>();		
+        rb = GetComponent<Rigidbody2D>();		
 	}
 
     void FixedUpdate() {
-        rb.AddForce(transform.forward * thrust);
+        rb.AddForce(transform.up * thrust);
+    }
+
+    void Update() {
+        target = FindClosestEnemy();
+        if(!target) {
+            return;
+        }
+        Vector3 dir = target.transform.position - transform.position;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
+    }
+
+    GameObject FindClosestEnemy() {
+        GameObject[] enemies;
+        enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        GameObject closest = null;
+        float distance = Mathf.Infinity;
+        Vector3 position = transform.position;
+        foreach (GameObject enemy in enemies) {
+            Vector3 diff = enemy.transform.position - position;
+            float curDistance = diff.sqrMagnitude;
+            if (curDistance < distance) {
+                closest = enemy;
+                distance = curDistance;
+            }
+        }
+        return closest;
     }
 }
